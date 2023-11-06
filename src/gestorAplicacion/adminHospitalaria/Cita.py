e47747040906b3b23609c699615502b9072e1744
banano <101402836+EsteArgen@users.noreply.github.com>
2023-11-06 00:14:32 -0500

    :-1:

--- src/gestorAplicacion/adminHospitalaria/Cita.py
+++ src/gestorAplicacion/adminHospitalaria/Cita.py
@@ -3,11 +3,6 @@ Autores: Maria P. Ardila, Jose N. Duque,
 Ronal Y. Castro, Daniela C. García y Leopold P. Lanard
 """
 
-from src.gestorAplicacion.sujeto.Medico import Medico
-from src.gestorAplicacion.sujeto.Paciente import Paciente
-from src.gestorAplicacion.serviciosOfrecidos.Tipo import Tipo
-from src.gestorAplicacion.sujeto.Categoria import Categoria
-from src.gestorAplicacion.sujeto.Categoria import Categoria
 # Descripción: Esta clase representa una Cita en el sistema de administración hospitalaria.
 
 
