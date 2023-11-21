acb6554d5107c5d32a74bd85e4526bb488e9e225
NICOXO <joduquel@unal.edu.co>
2023-11-21 00:09:47 -0500

    flop

--- excepciones/ErrorAplicacion.py
+++
@@ -1,5 +0,0 @@
-class ErrorAplicacion(Exception):
-
-    def __init__(self,mensaje):
-        self.mensaje_error_inicio = "Manejo de errores de la aplicacion:"+ mensaje
-        super().__init__(self.mensaje_error_inicio)
--- src/gestorAplicacion/serviciosOfrecidos/TipoTerapia.py
+++
@@ -1,9 +0,0 @@
-# Autores: Maria P. Ardila, Jose N. Duque, Ronal Y. Castro, Daniela C. García y Leopold P. Lanard
-
-# Descripción: Esta clase se encarga ...
-
-from enum import Enum
-
-class TipoTerapia(Enum):
-    ACONDICIONAMIENTO = 1
-    REHABILITACION = 2
